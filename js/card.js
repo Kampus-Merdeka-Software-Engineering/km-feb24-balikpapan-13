541e334c680a64894a08159a0a5c52e897d4cd95
Deni kristanto <crizzdeni4@gmail.com>
2024-06-06 15:20:09 +0700

    update card hilangkan rb dll

--- js/card.js
+++ js/card.js
@@ -3,19 +3,19 @@ fetch('superstore.json')
 .then(data => {
     // Menghitung total profit
     const totalProfit = data.reduce((sum, order) => sum + order['Profit'], 0);
-    document.getElementById('totalProfit').textContent = `$${totalProfit.toFixed(2)} rb`;
+    document.getElementById('totalProfit').textContent = `$${totalProfit.toFixed(2)} `;
 
     // Menghitung total sales
     const totalSales = data.reduce((sum, order) => sum + order['Sales'], 0);
-    document.getElementById('totalSales').textContent = `$${totalSales.toFixed(2)} jt`;
+    document.getElementById('totalSales').textContent = `$${totalSales.toFixed(2)} `;
 
     // Menghitung total order
     const totalOrders = data.length;
-    document.getElementById('totalOrders').textContent = `${totalOrders} rb`;
+    document.getElementById('totalOrders').textContent = `${totalOrders} `;
 
     // Menghitung unit terjual
     const unitsSold = data.reduce((sum, order) => sum + order['Quantity'], 0);
-    document.getElementById('unitsSold').textContent = `${unitsSold.toFixed(1)} rb`;
+    document.getElementById('unitsSold').textContent = `${unitsSold.toFixed(1)} `;
 
     // Menghitung jumlah customer
     const totalCustomers = new Set(data.map(order => order['Customer ID'])).size;
